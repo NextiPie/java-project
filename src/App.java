0cc3f5e8e6b6b3baa624dc1f4fa98a5d848736cc
NextiPie <smandarin90@gmail.com>
2025-04-21 21:35:37 +0300

    add exceptions

--- src/App.java
+++ src/App.java
@@ -1,8 +1,9 @@
 import java.util.*;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Author author = new Author("Толстой");
+        Author author1 = new Author("Пушкин");
 
         Book book = new Book("Война и мир", author);
         Book book1 = new Book("Преступление и наказание", author, "1866");
@@ -13,24 +14,24 @@ public class App {
         Library library = new Library();
 
         library.registerReader("Сергей", 13);
-        library.registerReader("Маша", 14);
+        //library.registerReader("Маша", 14);
 
         library.addBook("Война и мир", author);
-        library.addBook("Преступление и наказание", author);
+        //library.addBook("Преступление и наказание", author);
 
-        library.lendBook("Война и мир", "Сергей");
-        library.lendBook("Преступление и наказание", "Сергей");
+        //library.lendBook("Война и мир", "Сергей");
+        //library.lendBook("Преступление и наказание", "Сергей");
 
-        library.returnBook("Война и мир", "Сергей");
+        //library.returnBook("Война и мир", "Сергей");
 
         library.searchBook("Война и мир");
-        library.searchBook(author);
+        library.searchBook(author1);
 
-        library.listBorrowedBooks("Сергей");
+        //library.listBorrowedBooks("Сергей");
 
-        library.checkRemainingTime("Сергей");
+        //library.checkRemainingTime("Сергей");
 
-        library.popularityOfBook("Война и мир");
+        //library.popularityOfBook("Война и мир");
     }
 }
 
--- src/Library.java
+++ src/Library.java
@@ -1,13 +1,16 @@
+import javax.naming.AuthenticationException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-class Library {
+class Library{
     List<Book> books = new ArrayList<>();
     List<Reader> readers = new ArrayList<>();
     Map<Reader, List<Book>> loans = new HashMap<>();
     List<Integer> ides = new ArrayList<>();
+
+
 
     //Регистрация новых читателей с уникальным идентификатором
     void registerReader(String name, int idReaderForRegister) {
@@ -33,6 +36,7 @@ class Library {
 
     //Выдача книг читателям
     void lendBook(String bookTitle, String nameReader) {
+        boolean found = false;
         for (Book book : books) {
             if (book.title.equals(bookTitle) && book.isAvaileble) {
                 for (Reader reader : readers) {
@@ -42,17 +46,23 @@ class Library {
                         book.numberOfIssues += 1;
                         reader.setDateOfIssue(java.time.LocalDate.now());
                         loans.computeIfAbsent(reader, k -> new ArrayList<>()).add(book);
+                        found = true;
                         System.out.println("Книга " + "'" + book.title + "'" + " выдана читателю: " + reader.name + " | " + reader.getDateOfIssue());
-                        return;
                     }
                 }
             }
         }
-        System.out.println("Ошибка в названии книги или имени читателя");
+        try {
+            if (!found) throw new Exception("Ошибка в названии книги или имени читателя");
+        }
+        catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
     //Возврат книг
     void returnBook(String bookTitle, String nameReader) {
+        boolean found = false;
         for (Reader reader : readers) {
             if (reader.name.equals(nameReader)){
                 for (Book book : books) {
@@ -60,27 +70,51 @@ class Library {
                         book.isAvaileble = true;
                         loans.get(reader).remove(book);
                         System.out.println("Книга возвращена");
+                        found = true;
                     }
                 }
             }
+        }
+        try {
+            if (!found) throw new Exception("Ошибка в названии книги или в имени читателя");
+        }
+        catch(Exception ex) {
+            System.out.println(ex.getMessage());
         }
     }
 
     //Поиск книг по названию
     void searchBook(String titleBook) {
+        boolean found = false;
         for (Book book : books) {
             if (book.title.contains(titleBook) || book.author.name.contains(titleBook)) {
                 System.out.println("Найдена книга: " + book.title + " " + book.author.name);
+                found = true;
+
+            }
+            try {
+                if (!found) throw new Exception("Книга не найдена");
+            }
+            catch(Exception ex) {
+                System.out.println(ex.getMessage());
             }
         }
     }
 
     // Поиск по автору
-    void searchBook(Author authorName) {
+    void searchBook(Author authorName){
+        boolean found = false;
         for (Book book: books) {
-            if (book.author.equals(authorName)) {
+            if (book.author.equals(authorName)){
                 System.out.println("Найден автор: " + book.author + " " + book.title);
+                found = true;
             }
+        }
+        try {
+            if (!found) throw new Exception("Книга по автору не найдена");
+        }
+        catch(Exception ex) {
+            System.out.println(ex.getMessage());
         }
     }
 
@@ -89,12 +123,20 @@ class Library {
 
     //Список взятых книг у конкретного читателя
     void listBorrowedBooks(String nameReaders) {
+        boolean found = false;
         for (Reader reader : readers) {
             if (reader.name.equals(nameReaders)) {
                 for (int i = 0; i  < reader.borrowedBooks.size(); i++) {
                     System.out.println(reader.borrowedBooks.get(i));
+                    found = true;
                 }
             }
+        }
+        try {
+            if (!found) throw new Exception("Такой читатель не найден или имя введено неверно");
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
         }
     }
 
@@ -108,12 +150,20 @@ class Library {
     }
 
 
-
+    //Популярность книги
     void popularityOfBook(String titleBook) {
+        boolean found = false;
         for (Book book : books ) {
             if (book.title.equals(titleBook)) {
                 System.out.println(book.title + " брали " + book.numberOfIssues + " раз");
+                found = true;
             }
+        }
+        try {
+            if (!found) throw new Exception("Неверное название книги");
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
         }
     }
 
